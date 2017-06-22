9ad42da833eb28da4b3ac5589025947fdad9db56
Selen Uguroglu <suguroglu@hearst.com>
2017-06-22 09:56:01 -0400

    checks aroung output file size

---
+++ check_output_file_size.py
@@ -0,0 +1,19 @@
+import os
+from send_email import send_email
+is_debug = False
+
+try:
+    os.system("aws s3 ls --summarize --human-readable s3://hearstdataservices/buzzing/v4/mediaos_json_files.tar.gz > out.txt")
+
+    f = open("out.txt")
+    line = f.readline()
+    f.close()
+    size = line.split()[2]
+except:
+    size = -1
+mdict = {"Current file size": size}
+
+if float(size) < 4.:
+    send_email("BUZZING ALERT: OUTPUT FILE SIZE IS TOO SMALL",mdict,is_debug)
+else:
+    print("ALL OK")
